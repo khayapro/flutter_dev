038f652832e01fa8f9493c237cab44970e76b194
Khaya Mathebula <khayaprom@gmail.com>
2020-07-15 23:24:03 +0200

    added appBar and Image using Scaffold widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello Flutter'),
-      ),
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I am Rich!'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://img.favpng.com/15/17/20/blue-diamond-blue-diamond-png-favpng-KJupy4TMbyyyz7JF5ghUYy7Vf.jpg'),
+            ),
+          )),
     ),
   );
 }
