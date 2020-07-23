6d543c0974618c78da15a6cb3630f3cf4c4ce4cf
Khaya Mathebula <khayaprom@gmail.com>
2020-07-23 23:43:12 +0200

    created a listview and random names generated - part1 - complete

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -10,32 +10,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Welcome to my start-up app",
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: const Text("Start-up Namer"),
-        ),
-        body: Center(
-          child: RandomWords()
-        ),
-      ),
-    );
+      home: RandomWords());
   }
 }
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
-
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFontStyle = TextStyle(fontSize: 18, color: Colors.teal);
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Start-up name Generator"),
+        backgroundColor: Colors.teal,
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  /// Building a list view of random start-up names
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(18),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair suggestion) {
+    return ListTile(
+      title: Text(
+        suggestion.asPascalCase,
+        style: _biggerFontStyle,
+      ),
+    );
   }
 }
-
-
