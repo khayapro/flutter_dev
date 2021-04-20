dae139aa41ae69a59a23928ff3cb6db23fa217c6
Khaya Mathebula <khayaprom@gmail.com>
2021-04-20 18:53:41 +0200

    flutter: adding structural widgets i.e. Column

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -8,13 +8,28 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('App Header Title'),
-        ),
-        body: Center(
-          child: Text('Hello Center!!'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('App Header Title'),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Text('Whats your favourite color?'),
+                RaisedButton(
+                  child: Text('Answer 1'),
+                  onPressed: () => print('Button1 clicked! '),
+                ),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: () => print('Button2 clicked! '),
+                ),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: () => print('Button3 clicked! '),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
