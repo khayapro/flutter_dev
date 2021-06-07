a5090e3ebf6bf6dfb1633f2b37e5e3317d6e0a1a
Khaya Mathebula <khayaprom@gmail.com>
2021-06-07 19:59:44 +0200

    flutter: adding functions for interaction

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
+  void _buttonClick() {
+    print("Button pressed !");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,23 +16,31 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hello App"),
         ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: null,
-              child: Text("Button1"),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Text("Whats your question?"),
+                ElevatedButton(
+                  onPressed: _buttonClick,
+                  child: Text("Button1"),
+                ),
+                ElevatedButton(
+                  onPressed: _buttonClick,
+                  child: Text("Button2"),
+                ),
+                ElevatedButton(
+                  onPressed: _buttonClick,
+                  child: Text("Button3"),
+                )
+              ],
             ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text("Button2"),
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text("Button3"),
-            )
-          ],
+          ),
         ),
       ),
     );
+
+
   }
 }
